570bb9438c615f07792a31104dda32dfb12d7560
Klara Zelazko <klara.zelazko@gmail.com>
2026-07-26 19:22:51 +0200

    fix: correct company_logos import and rendering in TrustedBy

--- react-website/src/components/TrustedBy.jsx
+++ react-website/src/components/TrustedBy.jsx
@@ -3,10 +3,12 @@ import { company_logos } from '../assets/assets'
 
 const TrustedBy = () =>{
     return (
+         //sekcja z logotypami firm, wycentrowana
         <div className='flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80'>
             
             <h3 className = 'font-semibold'>Trusted by Innovative Companies</h3>
 
+            {/* rząd logotypów, zawija się na kolejne linie jeśli nie mieści się w jednej */}
             <div className = 'flex items-center justify-center flex-wrap gap-10 m-4'>
                 {company_logos.map((logo, index)=>(
                     <img key={index} src={logo} alt="" className='max-h-5 sm:max-h-6 dark:drop-shadow-xl'/>
